e63dfe899f4dcbd9e514f83b0e4898cef998eec2
Oversnatch <102408065+Oversnatch@users.noreply.github.com>
2022-03-26 06:02:16 -0300

    Add files via upload

---
+++ enums/FuncionName.java
@@ -0,0 +1,16 @@
+package com.americacg.cargavirtual.sube.enums;
+
+//=====================================================
+// FUNCIONES HABILITADAS EN EL GATEWAY
+//=====================================================
+// Funcion....: GCAT
+// Descripcion: Generacion Codigo de Acceso Token 
+//-----------------------------------------------------
+//Funcion....: VCAT
+//Descripcion: Verificacion Codigo de Acceso Token 
+//-----------------------------------------------------
+
+public enum FuncionName {
+	//Mnemonico de funciones habilitadas para operar en el gateway.
+	GCAT, VCAT
+}
