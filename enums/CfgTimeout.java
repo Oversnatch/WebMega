1e52afd282abd9c9eadded7bfbcb9d5f04c82e6e
Oversnatch <102408065+Oversnatch@users.noreply.github.com>
2022-03-26 06:08:32 -0300

    Add files via upload

---
+++ enums/CfgTimeout.java
@@ -0,0 +1,5 @@
+package com.americacg.cargavirtual.web.enums;
+
+public enum CfgTimeout {
+	DEFAULT, REPORTE, VENTA, CONSULTA
+}
---
+++ enums/OperacionEnEjecucion.java
@@ -0,0 +1,5 @@
+package com.americacg.cargavirtual.web.enums;
+
+public enum OperacionEnEjecucion {
+	DETALLE, ALTA, EDICION, BAJA
+}
